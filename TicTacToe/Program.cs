ceef3bd407bc845a2fa71a7bfdc0cbaabe3acd49
Angad Baidwan <angad.baidwan@outlook.com>
2023-01-04 18:17:37 -0500

    Updated spacing

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -10,7 +10,7 @@ namespace TicTacToe
             //UnitTesting.RunTest();
 
             // Greet
-            Console.WriteLine("Welcome to Console TicTacToe!");
+            Console.WriteLine(" Welcome to Console TicTacToe!");
             bool playAgain = false;
             do
             {
@@ -20,12 +20,12 @@ namespace TicTacToe
                 string validSymbolSelectionOptions = "xo";
                 do
                 {
-                    Console.WriteLine("Please enter which symbol you would like to play as ('o' or 'x'): ");
+                    Console.WriteLine(" Please enter which symbol you would like to play as ('o' or 'x'): ");
                     playerSymbolSelection = Console.ReadLine();
                     playerSymbolChar = playerSymbolSelection.Trim().ToLower()[0];
                     if (!validSymbolSelectionOptions.Contains(playerSymbolChar))
                     {
-                        Console.WriteLine("Sorry! Invalid input, please try again.");
+                        Console.WriteLine(" Sorry! Invalid input, please try again.");
                     }
                 } while (!validSymbolSelectionOptions.Contains(playerSymbolChar));
                 // Assign symbols to use and AI player
@@ -52,17 +52,17 @@ namespace TicTacToe
                 do
                 {
                     gameboard.PrintGameboard();
-                    Console.WriteLine("\nPlease enter your move by entering column number followed by row number (i.e. A1, C2, B3 etc.).");
+                    Console.WriteLine("\n Please enter your move by entering column number followed by row number (i.e. A1, C2, B3 etc.).");
                     playerMoveInput = Console.ReadLine().Trim().ToUpper();
                     try
                     {
                         playerMove = new GameSquare(playerMoveInput[0], (int)Char.GetNumericValue(playerMoveInput[1]), playerSymbol);
                         if (!gameboard.UpdateGameboard((GameSquare)playerMove))
-                            throw new Exception("Invalid player move.");
+                            throw new Exception(" Invalid player move.");
                     }
                     catch (Exception e)
                     {
-                        Console.WriteLine("\nInvalid move, please try again.\n");
+                        Console.WriteLine("\n Invalid move, please try again.\n");
                         continue;
                     }
                     // Check player win
@@ -89,13 +89,13 @@ namespace TicTacToe
                 switch (winner)
                 {
                     case 0:
-                        Console.WriteLine("\nThe outcome is a draw.\n");
+                        Console.WriteLine("\n The outcome is a draw.\n");
                         break;
                     case 1:
-                        Console.WriteLine("\nCongratulations! You won!\n");
+                        Console.WriteLine("\n Congratulations! You won!\n");
                         break;
                     case 2:
-                        Console.WriteLine("\nSorry, You lost.\n");
+                        Console.WriteLine("\n Sorry, You lost.\n");
                         break;
                 }
                 // Play again prompt
